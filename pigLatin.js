a5a77564794709e8bc6f453d2b9a1697925f0da3
Jason Reitsch <jreitsch@gmail.com>
2015-12-09 10:01:19 -0800

    Prompt to form.
    Working on changing from a prompt to an HTML form.

--- pigLatin.js
+++ pigLatin.js
@@ -1,26 +1,29 @@
-var word = prompt("Enter a word to be translated");
-var cons = []; // to gather the leading consonants
-word = word.toLowerCase();
-document.write("The word <i>" + word + "</i> translated into pig latin, looks like this:  ");
+$(document).ready(function () { 
+    function translateWord() {
+        var word = document.getElementById('word').value;
+        var cons = []; // to gather the leading consonants
+        word = word.toLowerCase();
+        var response = "The word <i>" + word + "</i> translated into pig latin, looks like this:  ";
 
-for (i = 0; i < word.length; i += 1) {
-    // Determine what position the first vowel is in, then take one route if it's in position 0 and the other if it comes after position 0.
-    
-    if (word[i] !== "a" && word[i] !== "e" && word[i] !== "i" && word[i] !== "o" && word[i] !== "u" && word[i] !== "y") {
-        // get each letter in the word, one at a time, and test for vowel. If it's a consonant, add it to the cons variable and iterate on.
-        cons += word[i];
-    } else {
-        // if it's a vowel, stop. Return position of first vowel.
-        if (i === 0) {
-            // If the word starts with a vowel, just append "way" to the end of the word.
-            document.write(word + "way");
-        } else {
-            // Move all consonants leading up to the first vowel to the end of the word and add an "ay" at the very end.
-            word = word.slice(i);
-            document.write("<b>" + word + cons + "ay</b>");
+        for (i = 0; i < word.length; i += 1) {
+            
+          // Iterate through letters. Add consonants to the cons variable. Stop on first vowel. Challenge text includes 'y' in vowels.
+            
+            if (word[i] !== "a" && word[i] !== "e" && word[i] !== "i" && word[i] !== "o" && word[i] !== "u" && word[i] !== "y") {
+                cons += word[i];
+            } else {
+                if (i === 0) {
+                  // From challange text - "If the word starts with a vowel, just append 'way' to the end of the word."
+                    response += word + "way";
+                } else {
+                  // From challenge text - "Move all consonants leading up to the first vowel to the end of the word and add an 'ay' at the very end."
+                    word = word.slice(i);
+                    response += "<b>" + word + cons + "ay</b>";
+                }
+                console.log(response);
+                document.write(response);
+            }
+
         }
-        
-        break;
     }
-
-}
+}); // end ready
